54c1792665c544faaea39b32055af55292346829
kong <459723919@qq.com>
2023-09-09 12:35:28 +0800

    Update config.ts
    Edit path

--- packages/backend/src/config.ts
+++ packages/backend/src/config.ts
@@ -8,4 +8,4 @@ export const config = {
 
 export const uploadPath = './upload/';
 export const projectRootDir =
-	'/Users/mac/腾讯/Macbook_tencent/Library/Containers/com.tencent.WeWorkMac/Data/WeDrive/腾讯/我的文件/技术和研发/公链方向学习/底链学习/BSN生态/NeptuneDigital-demo/packages/backend';
+	'/NeptuneDigital-demo/packages/backend';
